84d95f761c8ee4aa9897d193f08a5915c7ab711a
Mateusz Lesniak <matt.lesniak@gmail.com>
2021-12-07 13:19:07 +0100

    Adds days from 1 to 7

---
+++ day6.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let input = """
+1,1,3,5,1,3,2,1,5,3,1,4,4,4,1,1,1,3,1,4,3,1,2,2,2,4,1,1,5,5,4,3,1,1,1,1,1,1,3,4,1,2,2,5,1,3,5,1,3,2,5,2,2,4,1,1,1,4,3,3,3,1,1,1,1,3,1,3,3,4,4,1,1,5,4,2,2,5,4,5,2,5,1,4,2,1,5,5,5,4,3,1,1,4,1,1,3,1,3,4,1,1,2,4,2,1,1,2,3,1,1,1,4,1,3,5,5,5,5,1,2,2,1,3,1,2,5,1,4,4,5,5,4,1,1,3,3,1,5,1,1,4,1,3,3,2,4,2,4,1,5,5,1,2,5,1,5,4,3,1,1,1,5,4,1,1,4,1,2,3,1,3,5,1,1,1,2,4,5,5,5,4,1,4,1,4,1,1,1,1,1,5,2,1,1,1,1,2,3,1,4,5,5,2,4,1,5,1,3,1,4,1,1,1,4,2,3,2,3,1,5,2,1,1,4,2,1,1,5,1,4,1,1,5,5,4,3,5,1,4,3,4,4,5,1,1,1,2,1,1,2,1,1,3,2,4,5,3,5,1,2,2,2,5,1,2,5,3,5,1,1,4,5,2,1,4,1,5,2,1,1,2,5,4,1,3,5,3,1,1,3,1,4,4,2,2,4,3,1,1
+"""
+
+let fish = input.split(separator: ",")
+    .map(String.init)
+    .map { Int($0)! }
+
+func simulate(fish: [Int], after days: Int) {
+    var counters: [Int] = (0...8).map { _ in 0 }
+    Set(fish).forEach { counter in counters[counter] = fish.filter { $0 == counter }.count }
+    for _ in 1...days {
+        let newFish = counters.removeFirst()
+        counters.append(newFish)
+        counters[6] += newFish
+    }
+    print(counters.reduce(0, +))
+}
+
+let start = CFAbsoluteTimeGetCurrent()
+
+//simulate(fish: fish, after: 80)
+simulate(fish: fish, after: 256)
+
+let diff = CFAbsoluteTimeGetCurrent() - start
+print("\(#function) Took \(diff) seconds")
+
